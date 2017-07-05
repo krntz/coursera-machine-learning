aae30c3eecf59a1db3f57bab33057565f41fe53c
krntz <krntz@users.noreply.github.com>
2017-07-05 20:49:11 +0200

    Implemented regularized linear regression gradients

--- ex5/ex5.m
+++ ex5/ex5.m
@@ -48,13 +48,13 @@ m = size(X, 1);
 %
 
 theta = [1 ; 1];
-J = linearRegCostFunction([ones(m, 1) X], y, theta, 1);
+%J = linearRegCostFunction([ones(m, 1) X], y, theta, 1);
 
-fprintf(['Cost at theta = [1 ; 1]: %f '...
-         '\n(this value should be about 303.993192)\n'], J);
+%fprintf(['Cost at theta = [1 ; 1]: %f '...
+%         '\n(this value should be about 303.993192)\n'], J);
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+%fprintf('Program paused. Press enter to continue.\n');
+%pause;
 
 %% =========== Part 3: Regularized Linear Regression Gradient =============
 %  You should now implement the gradient for regularized linear 
