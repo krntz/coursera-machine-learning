3278b8d65a2bb1aa72dbe55b97d46685507b48df
krntz <krntz@users.noreply.github.com>
2017-07-06 20:58:54 +0200

    Finally implemented learning curves

--- ex5/ex5.m
+++ ex5/ex5.m
@@ -61,15 +61,15 @@ theta = [1 ; 1];
 %  regression.
 %
 
-theta = [1 ; 1];
-[J, grad] = linearRegCostFunction([ones(m, 1) X], y, theta, 1);
+%theta = [1 ; 1];
+%[J, grad] = linearRegCostFunction([ones(m, 1) X], y, theta, 1);
 
-fprintf(['Gradient at theta = [1 ; 1]:  [%f; %f] '...
-         '\n(this value should be about [-15.303016; 598.250744])\n'], ...
-         grad(1), grad(2));
+%fprintf(['Gradient at theta = [1 ; 1]:  [%f; %f] '...
+%         '\n(this value should be about [-15.303016; 598.250744])\n'], ...
+%         grad(1), grad(2));
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+%fprintf('Program paused. Press enter to continue.\n');
+%pause;
 
 
 %% =========== Part 4: Train Linear Regression =============
@@ -86,15 +86,15 @@ lambda = 0;
 [theta] = trainLinearReg([ones(m, 1) X], y, lambda);
 
 %  Plot fit over the data
-plot(X, y, 'rx', 'MarkerSize', 10, 'LineWidth', 1.5);
-xlabel('Change in water level (x)');
-ylabel('Water flowing out of the dam (y)');
-hold on;
-plot(X, [ones(m, 1) X]*theta, '--', 'LineWidth', 2)
-hold off;
+%plot(X, y, 'rx', 'MarkerSize', 10, 'LineWidth', 1.5);
+%xlabel('Change in water level (x)');
+%ylabel('Water flowing out of the dam (y)');
+%hold on;
+%plot(X, [ones(m, 1) X]*theta, '--', 'LineWidth', 2)
+%hold off;
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+%fprintf('Program paused. Press enter to continue.\n');
+%pause;
 
 
 %% =========== Part 5: Learning Curve for Linear Regression =============
--- ex5/linearRegCostFunction.m
+++ ex5/linearRegCostFunction.m
@@ -21,10 +21,12 @@ grad = zeros(size(theta));
 
 h = (X * theta);
 
-J = (1/(2 * m)) * sum((h - y) .^ 2) + (lambda/(2 * m))*sum(theta(2:end) .^ 2);
+J = (1/(2 * m)) * sum((h - y) .^ 2) + (lambda/(2 * m)) * sum(theta(2:end) .^ 2);
 
-grad = (1 / m) * sum((h - y) .* X);
-grad (2:end) += ((lambda / m) .* theta(2:end))';
+theta(1) = 0;
+grad = (1 / m) .*  X' * (h - y);
+grad = grad + ((lambda / m) .* theta);
+
 % =========================================================================
 
 grad = grad(:);
