d73aac3d73a2bfad8e295fb2fa13b8334f0608d7
krntz <krntz@users.noreply.github.com>
2017-07-08 14:35:19 +0200

    Implemented the validation curve

--- ex5/ex5.m
+++ ex5/ex5.m
@@ -110,20 +110,20 @@ lambda = 0;
                   [ones(size(Xval, 1), 1) Xval], yval, ...
                   lambda);
 
-plot(1:m, error_train, 1:m, error_val);
-title('Learning curve for linear regression')
-legend('Train', 'Cross Validation')
-xlabel('Number of training examples')
-ylabel('Error')
-axis([0 13 0 150])
+%plot(1:m, error_train, 1:m, error_val);
+%title('Learning curve for linear regression')
+%legend('Train', 'Cross Validation')
+%xlabel('Number of training examples')
+%ylabel('Error')
+%axis([0 13 0 150])
 
-fprintf('# Training Examples\tTrain Error\tCross Validation Error\n');
-for i = 1:m
-    fprintf('  \t%d\t\t%f\t%f\n', i, error_train(i), error_val(i));
-end
+%fprintf('# Training Examples\tTrain Error\tCross Validation Error\n');
+%for i = 1:m
+%    fprintf('  \t%d\t\t%f\t%f\n', i, error_train(i), error_val(i));
+%end
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+%fprintf('Program paused. Press enter to continue.\n');
+%pause;
 
 %% =========== Part 6: Feature Mapping for Polynomial Regression =============
 %  One solution to this is to use polynomial regression. You should now
@@ -149,11 +149,11 @@ X_poly_val = bsxfun(@minus, X_poly_val, mu);
 X_poly_val = bsxfun(@rdivide, X_poly_val, sigma);
 X_poly_val = [ones(size(X_poly_val, 1), 1), X_poly_val];           % Add Ones
 
-fprintf('Normalized Training Example 1:\n');
-fprintf('  %f  \n', X_poly(1, :));
+%fprintf('Normalized Training Example 1:\n');
+%fprintf('  %f  \n', X_poly(1, :));
 
-fprintf('\nProgram paused. Press enter to continue.\n');
-pause;
+%fprintf('\nProgram paused. Press enter to continue.\n');
+%pause;
 
 
 
@@ -164,36 +164,36 @@ pause;
 %  lambda to see how the fit and learning curve change.
 %
 
-lambda = 0;
+lambda = 100;
 [theta] = trainLinearReg(X_poly, y, lambda);
 
 % Plot training data and fit
-figure(1);
-plot(X, y, 'rx', 'MarkerSize', 10, 'LineWidth', 1.5);
-plotFit(min(X), max(X), mu, sigma, theta, p);
-xlabel('Change in water level (x)');
-ylabel('Water flowing out of the dam (y)');
-title (sprintf('Polynomial Regression Fit (lambda = %f)', lambda));
+%figure(1);
+%plot(X, y, 'rx', 'MarkerSize', 10, 'LineWidth', 1.5);
+%plotFit(min(X), max(X), mu, sigma, theta, p);
+%xlabel('Change in water level (x)');
+%ylabel('Water flowing out of the dam (y)');
+%title (sprintf('Polynomial Regression Fit (lambda = %f)', lambda));
 
-figure(2);
-[error_train, error_val] = ...
-    learningCurve(X_poly, y, X_poly_val, yval, lambda);
-plot(1:m, error_train, 1:m, error_val);
+%figure(2);
+%[error_train, error_val] = ...
+%    learningCurve(X_poly, y, X_poly_val, yval, lambda);
+%plot(1:m, error_train, 1:m, error_val);
 
-title(sprintf('Polynomial Regression Learning Curve (lambda = %f)', lambda));
-xlabel('Number of training examples')
-ylabel('Error')
-axis([0 13 0 100])
-legend('Train', 'Cross Validation')
+%title(sprintf('Polynomial Regression Learning Curve (lambda = %f)', lambda));
+%xlabel('Number of training examples')
+%ylabel('Error')
+%axis([0 13 0 100])
+%legend('Train', 'Cross Validation')
 
-fprintf('Polynomial Regression (lambda = %f)\n\n', lambda);
-fprintf('# Training Examples\tTrain Error\tCross Validation Error\n');
-for i = 1:m
-    fprintf('  \t%d\t\t%f\t%f\n', i, error_train(i), error_val(i));
-end
+%fprintf('Polynomial Regression (lambda = %f)\n\n', lambda);
+%fprintf('# Training Examples\tTrain Error\tCross Validation Error\n');
+%for i = 1:m
+%    fprintf('  \t%d\t\t%f\t%f\n', i, error_train(i), error_val(i));
+%end
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+%fprintf('Program paused. Press enter to continue.\n');
+%pause;
 
 %% =========== Part 8: Validation for Selecting Lambda =============
 %  You will now implement validationCurve to test various values of 
