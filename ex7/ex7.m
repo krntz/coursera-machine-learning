14b6d23003558574ecf8af4b3927503505120de9
krntz <krntz@users.noreply.github.com>
2017-07-23 15:51:01 +0200

    Implemented PCA

--- ex7/ex7.m
+++ ex7/ex7.m
@@ -29,15 +29,15 @@ clear ; close all; clc
 %fprintf('Finding closest centroids.\n\n');
 
 % Load an example dataset that we will be using
-load('ex7data2.mat');
+%load('ex7data2.mat');
 
 % Select an initial set of centroids
-K = 3; % 3 Centroids
-initial_centroids = [3 3; 6 2; 8 5];
+%K = 3; % 3 Centroids
+%initial_centroids = [3 3; 6 2; 8 5];
 
 % Find the closest centroids for the examples using the
 % initial_centroids
-idx = findClosestCentroids(X, initial_centroids);
+%idx = findClosestCentroids(X, initial_centroids);
 
 %fprintf('Closest centroids for the first 3 examples: \n')
 %fprintf(' %d', idx(1:3));
@@ -50,20 +50,20 @@ idx = findClosestCentroids(X, initial_centroids);
 %  After implementing the closest centroids function, you should now
 %  complete the computeCentroids function.
 %
-fprintf('\nComputing centroids means.\n\n');
+%fprintf('\nComputing centroids means.\n\n');
 
 %  Compute means based on the closest centroids found in the previous part.
-centroids = computeCentroids(X, idx, K);
+%centroids = computeCentroids(X, idx, K);
 
-fprintf('Centroids computed after initial finding of closest centroids: \n')
-fprintf(' %f %f \n' , centroids');
-fprintf('\n(the centroids should be\n');
-fprintf('   [ 2.428301 3.157924 ]\n');
-fprintf('   [ 5.813503 2.633656 ]\n');
-fprintf('   [ 7.119387 3.616684 ]\n\n');
+%fprintf('Centroids computed after initial finding of closest centroids: \n')
+%fprintf(' %f %f \n' , centroids');
+%fprintf('\n(the centroids should be\n');
+%fprintf('   [ 2.428301 3.157924 ]\n');
+%fprintf('   [ 5.813503 2.633656 ]\n');
+%fprintf('   [ 7.119387 3.616684 ]\n\n');
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+%fprintf('Program paused. Press enter to continue.\n');
+%pause;
 
 
 %% =================== Part 3: K-Means Clustering ======================
@@ -72,28 +72,28 @@ pause;
 %  kMeans algorithm. In this part, you will run the K-Means algorithm on
 %  the example dataset we have provided. 
 %
-fprintf('\nRunning K-Means clustering on example dataset.\n\n');
+%fprintf('\nRunning K-Means clustering on example dataset.\n\n');
 
 % Load an example dataset
-load('ex7data2.mat');
+%load('ex7data2.mat');
 
 % Settings for running K-Means
-K = 3;
-max_iters = 10;
+%K = 3;
+%max_iters = 10;
 
 % For consistency, here we set centroids to specific values
 % but in practice you want to generate them automatically, such as by
 % settings them to be random examples (as can be seen in
 % kMeansInitCentroids).
-initial_centroids = [3 3; 6 2; 8 5];
+%initial_centroids = [3 3; 6 2; 8 5];
 
 % Run K-Means algorithm. The 'true' at the end tells our function to plot
 % the progress of K-Means
-[centroids, idx] = runkMeans(X, initial_centroids, max_iters, true);
-fprintf('\nK-Means Done.\n\n');
+%[centroids, idx] = runkMeans(X, initial_centroids, max_iters, true);
+%fprintf('\nK-Means Done.\n\n');
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+%fprintf('Program paused. Press enter to continue.\n');
+%pause;
 
 %% ============= Part 4: K-Means Clustering on Pixels ===============
 %  In this exercise, you will use K-Means to compress an image. To do this,
@@ -123,7 +123,7 @@ X = reshape(A, img_size(1) * img_size(2), 3);
 
 % Run your K-Means algorithm on this data
 % You should try different values of K and max_iters here
-K = 16; 
+K = 32; 
 max_iters = 10;
 
 % When using K-Means, it is important the initialize the centroids
