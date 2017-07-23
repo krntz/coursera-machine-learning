9a8e0f5d17ba0c0d39c65609afcba11266e22734
krntz <krntz@users.noreply.github.com>
2017-07-23 16:14:52 +0200

    Something

--- ex7/ex7_pca.m
+++ ex7/ex7_pca.m
@@ -76,32 +76,32 @@ fprintf('\nRunning PCA on example dataset.\n\n');
 %
 %  You should complete the code in projectData.m
 %
-fprintf('\nDimension reduction on example dataset.\n\n');
+%fprintf('\nDimension reduction on example dataset.\n\n');
 
 %  Plot the normalized dataset (returned from pca)
-plot(X_norm(:, 1), X_norm(:, 2), 'bo');
-axis([-4 3 -4 3]); axis square
+%plot(X_norm(:, 1), X_norm(:, 2), 'bo');
+%axis([-4 3 -4 3]); axis square
 
 %  Project the data onto K = 1 dimension
-K = 1;
-Z = projectData(X_norm, U, K);
-fprintf('Projection of the first example: %f\n', Z(1));
-fprintf('\n(this value should be about 1.481274)\n\n');
+%K = 1;
+%Z = projectData(X_norm, U, K);
+%fprintf('Projection of the first example: %f\n', Z(1));
+%fprintf('\n(this value should be about 1.481274)\n\n');
 
-X_rec  = recoverData(Z, U, K);
-fprintf('Approximation of the first example: %f %f\n', X_rec(1, 1), X_rec(1, 2));
-fprintf('\n(this value should be about  -1.047419 -1.047419)\n\n');
+%X_rec  = recoverData(Z, U, K);
+%fprintf('Approximation of the first example: %f %f\n', X_rec(1, 1), X_rec(1, 2));
+%fprintf('\n(this value should be about  -1.047419 -1.047419)\n\n');
 
 %  Draw lines connecting the projected points to the original points
-hold on;
-plot(X_rec(:, 1), X_rec(:, 2), 'ro');
-for i = 1:size(X_norm, 1)
-    drawLine(X_norm(i,:), X_rec(i,:), '--k', 'LineWidth', 1);
-end
-hold off
+%hold on;
+%plot(X_rec(:, 1), X_rec(:, 2), 'ro');
+%for i = 1:size(X_norm, 1)
+%    drawLine(X_norm(i,:), X_rec(i,:), '--k', 'LineWidth', 1);
+%end
+%hold off
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+%fprintf('Program paused. Press enter to continue.\n');
+%pause;
 
 %% =============== Part 4: Loading and Visualizing Face Data =============
 %  We start the exercise by first loading and visualizing the dataset.
