03b2866f2f5474fa647556f31e6e6bbbf3b2cd94
krntz <krntz@users.noreply.github.com>
2017-07-23 16:02:40 +0200

    Implemented projectData

--- ex7/ex7_pca.m
+++ ex7/ex7_pca.m
@@ -55,17 +55,17 @@ fprintf('\nRunning PCA on example dataset.\n\n');
 
 %  Draw the eigenvectors centered at mean of data. These lines show the
 %  directions of maximum variations in the dataset.
-hold on;
-drawLine(mu, mu + 1.5 * S(1,1) * U(:,1)', '-k', 'LineWidth', 2);
-drawLine(mu, mu + 1.5 * S(2,2) * U(:,2)', '-k', 'LineWidth', 2);
-hold off;
+%hold on;
+%drawLine(mu, mu + 1.5 * S(1,1) * U(:,1)', '-k', 'LineWidth', 2);
+%drawLine(mu, mu + 1.5 * S(2,2) * U(:,2)', '-k', 'LineWidth', 2);
+%hold off;
 
-fprintf('Top eigenvector: \n');
-fprintf(' U(:,1) = %f %f \n', U(1,1), U(2,1));
-fprintf('\n(you should expect to see -0.707107 -0.707107)\n');
+%fprintf('Top eigenvector: \n');
+%fprintf(' U(:,1) = %f %f \n', U(1,1), U(2,1));
+%fprintf('\n(you should expect to see -0.707107 -0.707107)\n');
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+%fprintf('Program paused. Press enter to continue.\n');
+%pause;
 
 
 %% =================== Part 3: Dimension Reduction ===================
