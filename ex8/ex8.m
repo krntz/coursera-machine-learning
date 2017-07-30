526a2eec011f375f94f7009ce38592d602dfbd91
krntz <krntz@users.noreply.github.com>
2017-07-30 21:03:13 +0200

    Implemented F1 score

--- ex8/ex8.m
+++ ex8/ex8.m
@@ -27,20 +27,20 @@ clear ; close all; clc
 %  This exercise will help us find possibly faulty (or very fast) machines.
 %
 
-fprintf('Visualizing example dataset for outlier detection.\n\n');
+%fprintf('Visualizing example dataset for outlier detection.\n\n');
 
 %  The following command loads the dataset. You should now have the
 %  variables X, Xval, yval in your environment
 load('ex8data1.mat');
 
 %  Visualize the example dataset
-plot(X(:, 1), X(:, 2), 'bx');
-axis([0 30 0 30]);
-xlabel('Latency (ms)');
-ylabel('Throughput (mb/s)');
+%plot(X(:, 1), X(:, 2), 'bx');
+%axis([0 30 0 30]);
+%xlabel('Latency (ms)');
+%ylabel('Throughput (mb/s)');
 
-fprintf('Program paused. Press enter to continue.\n');
-pause
+%fprintf('Program paused. Press enter to continue.\n');
+%pause
 
 
 %% ================== Part 2: Estimate the dataset statistics ===================
@@ -51,7 +51,7 @@ pause
 %  both the overall distribution and where each of the points falls in 
 %  terms of that distribution.
 %
-fprintf('Visualizing Gaussian fit.\n\n');
+%fprintf('Visualizing Gaussian fit.\n\n');
 
 %  Estimate my and sigma2
 [mu sigma2] = estimateGaussian(X);
@@ -61,12 +61,12 @@ fprintf('Visualizing Gaussian fit.\n\n');
 p = multivariateGaussian(X, mu, sigma2);
 
 %  Visualize the fit
-visualizeFit(X,  mu, sigma2);
-xlabel('Latency (ms)');
-ylabel('Throughput (mb/s)');
+%visualizeFit(X,  mu, sigma2);
+%xlabel('Latency (ms)');
+%ylabel('Throughput (mb/s)');
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+%fprintf('Program paused. Press enter to continue.\n');
+%pause;
 
 %% ================== Part 3: Find Outliers ===================
 %  Now you will find a good epsilon threshold using a cross-validation set
@@ -76,21 +76,21 @@ pause;
 pval = multivariateGaussian(Xval, mu, sigma2);
 
 [epsilon F1] = selectThreshold(yval, pval);
-fprintf('Best epsilon found using cross-validation: %e\n', epsilon);
-fprintf('Best F1 on Cross Validation Set:  %f\n', F1);
-fprintf('   (you should see a value epsilon of about 8.99e-05)\n');
-fprintf('   (you should see a Best F1 value of  0.875000)\n\n');
+%fprintf('Best epsilon found using cross-validation: %e\n', epsilon);
+%fprintf('Best F1 on Cross Validation Set:  %f\n', F1);
+%fprintf('   (you should see a value epsilon of about 8.99e-05)\n');
+%fprintf('   (you should see a Best F1 value of  0.875000)\n\n');
 
 %  Find the outliers in the training set and plot the
 outliers = find(p < epsilon);
 
 %  Draw a red circle around those outliers
-hold on
-plot(X(outliers, 1), X(outliers, 2), 'ro', 'LineWidth', 2, 'MarkerSize', 10);
-hold off
+%hold on
+%plot(X(outliers, 1), X(outliers, 2), 'ro', 'LineWidth', 2, 'MarkerSize', 10);
+%hold off
 
-fprintf('Program paused. Press enter to continue.\n');
-pause;
+%fprintf('Program paused. Press enter to continue.\n');
+%pause;
 
 %% ================== Part 4: Multidimensional Outliers ===================
 %  We will now use the code from the previous part and apply it to a 
